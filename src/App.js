cacfca79d39ca914e1482715b71160b767fa1bc2
vickneee <victoria.vavulina@outlook.com>
2023-05-15 21:48:25 +0300

    ClassName corr

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to my site</h1>
+        <h1 className="App-h1">Welcome to my site</h1>
         <p>This is my first React app</p>
       </header>
     </div>
