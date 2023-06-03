3642ff533b7906f5c7c5311e558996f4e8129fc5
vickneee <victoria.vavulina@outlook.com>
2023-06-03 23:10:38 +0300

    Corrections

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Card from "./Card";
+import App from "./App";
+import CardList from "./CardList";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
-import { robots } from "./robots";
+import { robots } from "./Robots";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-    <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} />
-    <Card id={robots[4].id} name={robots[4].name} email={robots[4].email} />
-    <Card id={robots[5].id} name={robots[5].name} email={robots[5].email} />
+    <CardList robots={robots} />
   </React.StrictMode>
 );
 
