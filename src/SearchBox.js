10d28a216f15deb033bca0d37cb5fc2a5ba2f6ae
vickneee <victoria.vavulina@outlook.com>
2023-06-04 02:21:29 +0300

    SearchBox

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const SearchBox = () => {
-  return <input type="search" placeholder="Search robots" />;
+  return (
+    <div className="pa2">
+      <input
+        className="pa3 ba b--green bg-washed-green f4"
+        type="search"
+        placeholder="Search robots"
+      />
+    </div>
+  );
 };
 
 export default SearchBox;
