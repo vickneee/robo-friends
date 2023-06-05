d925a76f421ffa61b900ffc510e0073ef69f78fb
vickneee <victoria.vavulina@outlook.com>
2023-06-06 02:54:40 +0300

    SearchBox letter spacing

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,10 +1,11 @@
 import React from "react";
+import './SearchBox.css';
 
 const SearchBox = ({ searchChange }) => {
   return (
     <div className="pa2">
       <input
-        className="pa3 ba br3 b--green bg-lightest-blue f5"
+        className="pa3 ba br3 bg-lightest-blue"
         type="search"
         placeholder="Search robots"
         onChange={searchChange}
